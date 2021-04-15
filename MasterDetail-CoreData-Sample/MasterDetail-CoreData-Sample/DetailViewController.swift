77e1f7b2b247935a363f54c103c679ad6d28c2e4
Kim Jung <godrm77@gmail.com>
2021-04-15 12:49:37 +0900

    Initialize Project from Template

---
+++ MasterDetail-CoreData-Sample/MasterDetail-CoreData-Sample/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  MasterDetail-CoreData-Sample
+//
+//  Created by JK on 2021/04/15.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
